fc532afdd14c3ccd7218a08c8b41d39c3ee8ef06
wissSc <153686200+wissSc@users.noreply.github.com>
2025-06-04 21:09:29 -0300

    Update api.js

--- GerenciamentoDeEstacionamento/frontend/src/services/api.js
+++ GerenciamentoDeEstacionamento/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: 'https://gerenciamentodeestacionamento.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
